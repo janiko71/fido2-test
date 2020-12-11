7db3d4b7fb8af1de6ae485a47ec759828e6d91af
janiko71 <janiko@geba.fr>
2020-12-11 17:13:35 +0100

    commentaires ajoutés pour la définition des champs

--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 import os, sys, logging
+import signal
 import json
 import binascii, base64
 import argparse
@@ -87,13 +88,14 @@ def read_device_jwt(url, token):
 
     try:
 
+        logging.info("     | Calling for device information : " + Fore.LIGHTWHITE_EX + "{}".format(curl_url))
         u = requests.get(curl_url, allow_redirects=True)
 
         status_code = u.status_code
         reason      = u.reason
         content     = u.content
-        logging.info("Status code : " + Fore.LIGHTWHITE_EX + "{} ({})".format(status_code, reason))
-        logging.info("Content lenght (encoded) : {} bytes".format(len(content)))
+        logging.info("     | Device Status code : " + Fore.LIGHTWHITE_EX + "{} ({})".format(status_code, reason))
+        logging.info("     | Device Content lenght (encoded) : {} bytes".format(len(content)))
 
         if (status_code == 200):
             # Got an answer, no problem
@@ -214,7 +216,7 @@ def decode_jwt(data, token):
         '''
         decode_device_jwt(device_jwt)
 
-    logging.info("Legal : " + Fore.LIGHTWHITE_EX + jd['legalHeader'])
+    logging.info("Legal : " + Fore.LIGHTWHITE_EX + jd['legalHeader'][:100] + "...")
 
 
 def decode_device_jwt(data):
@@ -225,9 +227,282 @@ def decode_device_jwt(data):
 
     base64_bytes = base64.b64decode(data, '-_') # Some infos are strangely encoded...
     device = json.loads(base64_bytes)
-    for k in device.keys():
-        str_format = "Device | {} : " + Fore.LIGHTWHITE_EX + "{}"
-        logging.info(str_format.format(k, device.get(k)))
+
+    str_format = "     | Device {} : " + Fore.LIGHTWHITE_EX + "{}"
+
+    #
+    # Displaying information
+    #
+    # Reference : https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#idl-def-MetadataStatement
+    #
+    # Values are defined in a FIDO registry : https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-registry-v2.0-rd-20180702.html
+    # 
+
+    # Key: aaid
+    # ---
+    # See https://fidoalliance.org/specs/fido-uaf-v1.2-rd-20171128/fido-uaf-protocol-v1.2-rd-20171128.html#authenticator-attestation-id-aaid-typedef
+    #
+    # Authenticator Attestation ID. This field MUST be set if the authenticator implements FIDO UAF.
+    # The AAID is a string with format "V#M", where
+    #   "#" is a separator
+    #   "V" indicates the authenticator Vendor Code. This code consists of 4 hexadecimal digits.
+    #   "M" indicates the authenticator Model Code. This code consists of 4 hexadecimal digits.
+    #
+
+    key = 'aaid'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+    # Key: description 
+    # ---
+    # Human-readable, short description of the authenticator, in English)
+    #
+
+    key = 'description'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+    # Key: isSecondFactorOnly
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-isSecondFactorOnly
+    #
+    # Indicates if the authenticator is designed to be used only as a second factor, i.e. requiring some other authentication method as a first factor (e.g. username+password).
+    #
+
+    key = 'isSecondFactorOnly'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+    # Key: matcherProtection
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-matcherProtection
+    #
+    # A 16-bit number representing the bit fields defined by the MATCHER_PROTECTION constants in the FIDO Registry of Predefined Values
+    #
+    #   MATCHER_PROTECTION_SOFTWARE 0x0001 
+    #       This flag must be set if the authenticator's matcher is running in software. Exclusive in authenticator metadata with MATCHER_PROTECTION_TEE, MATCHER_PROTECTION_ON_CHIP
+    #   MATCHER_PROTECTION_TEE 0x0002
+    #       This flag should be set if the authenticator's matcher is running inside the Trusted Execution Environment [TEE]. Mutually exclusive in authenticator metadata with MATCHER_PROTECTION_SOFTWARE, MATCHER_PROTECTION_ON_CHIP
+    #   MATCHER_PROTECTION_ON_CHIP 0x0004
+    #       This flag should be set if the authenticator's matcher is running on the chip. Mutually exclusive in authenticator metadata with MATCHER_PROTECTION_TEE, MATCHER_PROTECTION_SOFTWARE
+    # 
+
+    key = 'matcherProtection'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+    # Key: protocolFamily
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-protocolFamily
+    #
+    # The FIDO protocol family. The values "uaf", "u2f", and "fido2" are supported. If this field is missing, the assumed protocol family is "uaf". Metadata Statements for
+    # U2F authenticators must set the value of protocolFamily to "u2f" and FIDO 2.0/WebAuthentication Authenticator implementations must set the value of protocolFamily to "fido2".
+    # 
+
+    key = 'protocolFamily'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+    # Key: upv
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-upv
+    #
+    # The FIDO unified protocol version(s) (related to the specific protocol family) supported by this authenticator. See [UAFProtocol] for the definition of the Version structure.
+    # 
+
+    key = 'upv'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+    # Key: tcDisplay (transaction confirmation display)
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-tcDisplay
+    #
+    # A 16-bit number representing a combination of the bit flags defined by the TRANSACTION_CONFIRMATION_DISPLAY constants in the FIDO Registry of Predefined Values [FIDORegistry].
+    # This value must be 0, if transaction confirmation is not supported by the authenticator.
+    #
+
+    key = 'tcDisplay'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: tcDisplayContentType
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-tcDisplayContentType
+    #
+    # Supported MIME content type [RFC2049] for the transaction confirmation display, such as text/plain or image/png.
+    # This value must be present if transaction confirmation is supported, i.e. tcDisplay is non-zero.
+    # 
+
+    key = 'tcDisplayContentType'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: userVerificationDetails
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-userVerificationDetails
+    #
+    # A list of alternative VerificationMethodANDCombinations. Each of these entries is one alternative user verification method. Each of these alternative user verification 
+    # methods might itself be an "AND" combination of multiple modalities.
+    # All effectively available alternative user verification methods must be properly specified here. A user verification method is considered effectively available if this 
+    # method can be used to either:
+    #  - enroll new verification reference data to one of the user verification methods
+    #  - unlock the UAuth key directly after successful user verification
+    # 
+
+    key = 'userVerificationDetails'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: assertionScheme
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-assertionScheme
+    #
+    # A list of alternative VerificationMethodANDCombinations. Each of these entries is one alternative user verification method. Each of these alternative user verification methods 
+    # might itself be an "AND" combination of multiple modalities.
+    # All effectively available alternative user verification methods must be properly specified here. A user verification method is considered effectively available if this method 
+    # can be used to either:
+    #   - enroll new verification reference data to one of the user verification methods 
+    #   - unlock the UAuth key directly after successful user verification
+    # 
+
+    key = 'assertionScheme'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: attachmentHint
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-attachmentHint
+    #
+    # A 32-bit number representing the bit fields defined by the ATTACHMENT_HINT constants in the FIDO Registry of Predefined Values. The ATTACHMENT_HINT constants are flags 
+    # in a bit field represented as a 32 bit long. They describe the method an authenticator uses to communicate with the FIDO User Device. These constants are reported and 
+    # queried through the UAF Discovery APIs [UAFAppAPIAndTransport], and used to form Authenticator policies in UAF protocol messages. Because the connection state and topology 
+    # of an authenticator may be transient, these values are only hints that can be used by server-supplied policy to guide the user experience, e.g. to prefer a device that is 
+    # connected and ready for authenticating or confirming a low-value transaction, rather than one that is more secure but requires more user effort.
+    #
+    # ATTACHMENT_HINT_INTERNAL 0x0001
+    #	This flag may be set to indicate that the authenticator is permanently attached to the FIDO User Device.A device such as a smartphone may have authenticator functionality 
+    #   that is able to be used both locally and remotely. In such a case, the FIDO client must filter and exclusively report only the relevant bit during Discovery and when 
+    #   performing policy matching. This flag cannot be combined with any other ATTACHMENT_HINT flags.
+    # ATTACHMENT_HINT_EXTERNAL 0x0002
+    #	This flag may be set to indicate, for a hardware-based authenticator, that it is removable or remote from the FIDO User Device. A device such as a smartphone may 
+    #   have authenticator functionality that is able to be used both locally and remotely. In such a case, the FIDO UAF Client must filter and exclusively report only the 
+    #   relevant bit during discovery and when performing policy matching. This flag must be combined with one or more other ATTACHMENT_HINT flag(s).
+    # ATTACHMENT_HINT_WIRED 0x0004
+    #	This flag may be set to indicate that an external authenticator currently has an exclusive wired connection, e.g. through USB, Firewire or similar, to the FIDO User Device.
+    # ATTACHMENT_HINT_WIRELESS 0x0008
+    #	This flag may be set to indicate that an external authenticator communicates with the FIDO User Device through a personal area or otherwise non-routed wireless protocol, 
+    #   such as Bluetooth or NFC.
+    # ATTACHMENT_HINT_NFC 0x0010
+    #	This flag may be set to indicate that an external authenticator is able to communicate by NFC to the FIDO User Device. As part of authenticator metadata, or when reporting 
+    #   characteristics through discovery, if this flag is set, the ATTACHMENT_HINT_WIRELESS flag should also be set as well.
+    # ATTACHMENT_HINT_BLUETOOTH 0x0020
+    #	This flag may be set to indicate that an external authenticator is able to communicate using Bluetooth with the FIDO User Device. As part of authenticator metadata, or 
+    #   when reporting characteristics through discovery, if this flag is set, the ATTACHMENT_HINT_WIRELESS flag should also be set.
+    # ATTACHMENT_HINT_NETWORK 0x0040
+    #	This flag may be set to indicate that the authenticator is connected to the FIDO User Device over a non-exclusive network (e.g. over a TCP/IP LAN or WAN, as opposed 
+    #   to a PAN or point-to-point connection).
+    # ATTACHMENT_HINT_READY 0x0080
+    #	This flag may be set to indicate that an external authenticator is in a "ready" state. This flag is set by the ASM at its discretion. 
+    #
+
+    key = 'attachmentHint'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: attestationRootCertificates
+    # ---
+    # Each element of this array represents a X.509 certificate that is a valid trust anchor for this authenticator model. The array does not represent a certificate chain, but 
+    # only the trust anchor of that chain. 
+    #
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-attestationRootCertificates
+    #
+
+    key = 'attestationRootCertificates'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: attestationTypes
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-attestationTypes
+    #
+    # The supported attestation type(s). (e.g. ATTESTATION_BASIC_FULL(0x3E07), ATTESTATION_BASIC_SURROGATE(0x3E08)).
+    # 
+
+    key = 'attestationTypes'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: authenticationAlgorithm
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-authenticationAlgorithm
+    # 
+    # The preferred authentication algorithm supported by the authenticator. This value must be non-zero.
+    #
+
+    key = 'authenticationAlgorithm'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: authenticatorVersion
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-authenticatorVersion
+    #
+    # Earliest (i.e. lowest) trustworthy authenticatorVersion meeting the requirements specified in this metadata statement.
+    # 
+
+    key = 'authenticatorVersion'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+    # Key: icon
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-icon
+    #
+    # RFC2397-encoded PNG icon for the Authenticator.
+    # 
+
+    key = 'icon'
+    if (key in device):
+        logging.info(str_format.format(key, "found"))
+    else:
+        logging.info(str_format.format(key, "not found"))
+
+    # Key: legalHeader
+    # ---
+    # See https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#widl-MetadataStatement-legalHeader
+    #
+    # The legalHeader, if present, contains a legal guide for accessing and using metadata, which itself may contain URL(s) pointing 
+    # to further information, such as a full Terms and Conditions statement.
+    # 
+
+    key = 'legalHeader'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)[:100] + "..."))
+
+    # Key: publicKeyAlgAndEncoding
+    # ---
+    #
+    # 
+
+    key = 'publicKeyAlgAndEncoding'
+    if (key in device):
+        logging.info(str_format.format(key, device.get(key)))
+
+
+
+def signal_handler(sig, frame):
+
+    # Just to prevent verbose aborting
+
+    print('\nCtrl+C detected, aborting silently...')
+    sys.exit(0)
 
 
 def main(argv):
@@ -271,6 +546,7 @@ def main(argv):
 if __name__ == "__main__":
 
     print()
+    signal.signal(signal.SIGINT, signal_handler)
     try:
         main(sys.argv[1:])
     finally:
